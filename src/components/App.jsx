bf102144e6482ec6b70b292861e6432608fcf0ba
Yevhenii Pozniak <jekamanu@gmail.com>
2023-01-23 10:35:28 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,52 @@
 import { Component } from 'react';
-import { ContactForm } from './Form/Form.jsx';
-
+import { Form, FormLabel, FormInput, FormButton, Title } from './Form/Form';
 export class App extends Component {
   state = {
     contacts: [],
     name: '',
   };
 
+  addTodo = text => {
+    console.log(text);
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+    this.reset();
+  };
+
+  handleChange = event => {
+    this.setState({ name: event.currentTarget.value });
+  };
+
+  reset = () => {
+    this.setState({ name: '' });
+  };
+
   render() {
+    const { name } = this.state;
+    const { handleSubmit, handleChange } = this;
+
     return (
-      <>
-        <ContactForm />
-      </>
+      <div>
+        <Title>Phonebook</Title>
+        <Form onSubmit={handleSubmit}>
+          <FormLabel>
+            Name
+            <FormInput
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={name}
+              onChange={handleChange}
+            />
+          </FormLabel>
+          <FormButton type="submit">Add contact</FormButton>
+        </Form>
+      </div>
     );
   }
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,25 +1,48 @@
-import { Component } from 'react';
-import { Form, FormLabel, FormInput, FormButton, Title } from './Form';
+// import { Component } from 'react';
+// import { Form, FormLabel, FormInput, FormButton, Title } from './Form';
 
-export class ContactForm extends Component {
-  render() {
-    return (
-      <div>
-        <Title>Phonebook</Title>
-        <Form>
-          <FormLabel>
-            Name
-            <FormInput
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </FormLabel>
-          <FormButton type="submit">Add contact</FormButton>
-        </Form>
-      </div>
-    );
-  }
-}
+// export class ContactForm extends Component {
+//   state = {
+//     inputValue: '',
+//   };
+
+//   onHandleSubmit = event => {
+//     event.preventDefault();
+//     console.log(event.currentTarget.value);
+//     this.reset();
+//   };
+
+//   onhandleChange = event => {
+//     this.setState({ inputValue: event.target.value });
+//   };
+
+//   reset = () => {
+//     this.setState({ inputValue: '' });
+//   };
+
+//   render() {
+//     const { inputValue } = this.state;
+//     const { onHandleSubmit, onhandleChange } = this;
+
+//     return (
+//       <div>
+//         <Title>Phonebook</Title>
+//         <Form onSubmit={onHandleSubmit}>
+//           <FormLabel>
+//             Name
+//             <FormInput
+//               type="text"
+//               name="name"
+//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//               required
+//               value={inputValue}
+//               onChange={onhandleChange}
+//             />
+//           </FormLabel>
+//           <FormButton type="submit">Add contact</FormButton>
+//         </Form>
+//       </div>
+//     );
+//   }
+// }
